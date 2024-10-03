ac69b3de803031607272e95b0eb8e9aa9386eea1
SumiyaRahman <sumiarahman01739@gmail.com>
2024-10-03 17:36:20 +0600

    card description

--- scripts/video.js
+++ scripts/video.js
@@ -1,3 +1,11 @@
+// get time
+function getTimeString(time) {
+  const hour = parseInt(time / 3600);
+  const remainingSecond = time % 3600;
+  const minute = parseInt(remainingSecond / 60);
+  return `${hour}hrs ${minute} min ago`;
+}
+
 // 1 - Fetch, Load and Show Categories on html
 
 // Create loadCategories
@@ -18,24 +26,24 @@ const loadVideos = () => {
     .catch((error) => console.log(error));
 };
 
-const cardDemo = {
-    "category_id": "1001",
-    "video_id": "aaaa",
-    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-    "title": "Shape of You",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-            "profile_name": "Olivia Mitchell",
-            "verified": ""
-        }
-    ],
-    "others": {
-        "views": "100K",
-        "posted_date": "16278"
-    },
-    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-}
+// const cardDemo = {
+//     "category_id": "1001",
+//     "video_id": "aaaa",
+//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//     "title": "Shape of You",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//             "profile_name": "Olivia Mitchell",
+//             "verified": ""
+//         }
+//     ],
+//     "others": {
+//         "views": "100K",
+//         "posted_date": "16278"
+//     },
+//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// }
 // create display videos
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videos");
@@ -47,19 +55,36 @@ const displayVideos = (videos) => {
     // creating video card
     const card = document.createElement("div");
     card.classList = "card card-compact";
-    card.innerHTML = `<figure>
+    card.innerHTML = `<figure class = "h-[200px] relative">
     <img
       src= ${video.thumbnail}
-      alt="Shoes" />
+      class = "h-full w-full object-fit" />
+      ${
+        video.others.posted_date?.length == 0
+          ? ""
+          : `<span class = "absolute right-2 bottom-2 bg-[#171717] rounded text-white p-1 text-[10px]">${getTimeString(video.others.posted_date)}</span>`
+      }
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="px-0 py-2 flex gap-3">
+    <div>
+    <img class = "w-10 h-10 rounded-full object-cover" src = ${
+      video.authors[0].profile_picture
+    } />
+    </div>
+    <div>
+    <h2 class = "text-[#171717] font-bold text-base">${video.title}</h2>
+    <div class = "flex items-center gap-2">
+    <p class = "text-[#171717b3] text-sm">${video.authors[0].profile_name}</p>
+    ${
+      video.authors[0].verified == true
+        ? `<img src = "../assets/verified.png">`
+        : " "
+    }
+    </div>
+    <p></p>
     </div>
   </div>`;
-  videoContainer.append(card);
+    videoContainer.append(card);
   });
 };
 // Create displayCategories
